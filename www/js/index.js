d8ac39705384a758f362bc648ec778ef16880e76
Patrycja Kazek <patrycja.op839@gmail.com>
2018-04-22 18:00:21 +0200

    weather api

--- www/js/index.js
+++ www/js/index.js
@@ -1,15 +1,19 @@
-
-    
-$("#getWeather").click(function(){
-    var city = $("#city").val();
-    var key = '33dbe3b930c23ad2c7a0630b49f3e440';   
-    
-    $.get('http://api.openweathermap.org/data/2.5/weather', {q:city, appid:key, units: 'metric'},  function(data) {
-            var zm = '';
-            $.each(data.weather, function(index, val){
-                zm += '<p><b>' + data.name + "</b></p>" + data.main.temp + '&deg;C' + '|' + val.main + ", " + val.description
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    alert("yo!");
+        
+            $("#getWeather").click(function(){
+                var city = $("#city").val();
+                var key = '33dbe3b930c23ad2c7a0630b49f3e440';   
+                
+                $.get('http://api.openweathermap.org/data/2.5/weather', {q:city, appid:key, units: 'metric'},  function(data) {
+                        var zm = '';
+                        $.each(data.weather, function(index, val){
+                            zm += '<p><b>' + data.name + "</b></p>" + data.main.temp + '&deg;C' + '|' + val.main + ", " + val.description
+                        });
+                        $("#show").html(zm);
+                    }, 'json');         
             });
-            $("#show").html(zm);
-        }, 'json');         
-});
-      
+       
+    
+}
