6b0fcff24a09795aaafc07e4bcb45c13d18b8637
Patrycja Kazek <patrycja.op839@gmail.com>
2018-04-22 14:26:00 +0200

    camera plugin

--- www/js/index.js
+++ www/js/index.js
@@ -1,54 +1,11 @@
 
-document.addEventListener("offline", onOffline, false);
-document.addEventListener("online", onOnline, false);
-
-function onOffline() {
-   alert('You are now offline!');
-};
-
-function onOnline() {
-   alert('You are now online!');
-};
-
-
-
-
-function setOptions(srcType) {
-    var options = {
-        // Some common settings are 20, 50, and 100
-        quality: 50,
-        destinationType: Camera.DestinationType.FILE_URI,
-        // In this app, dynamically set the picture source, Camera or photo gallery
-        sourceType: srcType,
-        encodingType: Camera.EncodingType.JPEG,
-        mediaType: Camera.MediaType.PICTURE,
-        allowEdit: true,
-        correctOrientation: true  //Corrects Android orientation quirks
-    }
-    return options;
-};
-
-
-function openCamera(selection) {
- 
-    var srcType = Camera.PictureSourceType.CAMERA;
-    var options = setOptions(srcType);
-    var func = createNewFileEntry;
- 
-    navigator.camera.getPicture(function cameraSuccess(imageUri) {
-        alert("Usucess")
-        displayImage(imageUri);
-        // You may choose to copy the picture, save it somewhere, or upload.
-        //func(imageUri);
- 
-    }, function cameraError(error) {
-        alert("Unable to obtain picture: " + error);
- 
-    }, options);
-};
-
-function displayImage(imgUri) {
- 
-    var elem = document.getElementById('imageFile');
-    elem.src = imgUri;
-};
+navigator.camera.getPicture(onSuccess, onFail, { quality: 50, 
+    destinationType: Camera.DestinationType.DATA_URL});
+function onSuccess(imageData){
+    var image = document.getElementById('imageFile');
+    image.src = "data:image/jpeg;base64," +imageData;
+    alert("image updated");
+}
+function onFail(message){
+    alert("fail " + message);
+}
