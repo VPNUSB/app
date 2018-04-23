afca2a97e4fdc0c947698eb1e0b98040d446d5d4
Patrycja Kazek <patrycja.op839@gmail.com>
2018-04-23 13:53:56 +0200

    login google

--- www/js/index.js
+++ www/js/index.js
@@ -14,6 +14,33 @@ function onDeviceReady() {
                         $("#show").html(zm);
                     }, 'json');         
             });
-       
+   
+var provider = new firebase.auth.GoogleAuthProvider();
+var user;
+
+function signIn() {
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      user = result.user;
+      alert("info" + result.user);
+      // ...
+    }).catch(function(error) {
+        alert("error!" + error.message);
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+
+};
+
+
+
     
 }
